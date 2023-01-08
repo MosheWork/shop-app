b5863ba19420b6d73cce2bd43f4d5236b9a18bf1
moshe maman <moshework1985@gmail.com>
2023-01-08 10:33:38 +0200

    counter working

--- src/app/items/items.model.ts
+++ src/app/items/items.model.ts
@@ -3,11 +3,19 @@ export class Item {
   public price: number;
   public description: string;
   public pic: string;
+  public counter: number;
 
-  constructor(name: string, price: number, description: string, pic: string) {
+  constructor(
+    name: string,
+    price: number,
+    counter: number,
+    description: string,
+    pic: string
+  ) {
     this.name = name;
     this.price = price;
     this.description = description;
     this.pic = pic;
+    this.counter = counter;
   }
 }
